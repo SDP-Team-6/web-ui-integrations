fa76f8dc43513d574c0e095bdf51f70f66e043d8
GaganSD <gagandevagiri@gmail.com>
2021-03-12 09:20:53 +0000

    accesibility feature - show/hide password

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -18,6 +18,10 @@ def settings():
 def login():
     return render_template("login.html")
 
+@app.route("/main")
+def main():
+    return render_template("head.html")
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ flaskapp/static/js/app.js
@@ -0,0 +1,8 @@
+$(document).on('click', '.toggle-password', function() {
+
+    $(this).toggleClass("fa-eye fa-eye-slash");
+    
+    var input = $("#password_box");
+    input.attr('type') === 'password' ? input.attr('type','text') : input.attr('type','password')
+
+});
