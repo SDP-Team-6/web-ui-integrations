441e7da90029ef35d531aeb403573c44d247d93d
abc12345d <zhiqi31@gmail.com>
2021-03-21 20:54:36 +0000

    toggle password feature

---
+++ website/js/login.js
@@ -0,0 +1,8 @@
+function togglePassword() {
+    var x = document.getElementById("password-field");
+    if (x.type == "password") {
+      x.type = "text";
+    } else {
+      x.type = "password";
+    }
+  } 
