e730433e0fef5ee346d49a60167e30e614f0310f
Jim <49323558+Jimsu7@users.noreply.github.com>
2021-03-22 21:20:31 +0800

    The flask passes arguments to the robot
    The flask passes arguments on the setting page to the robot. This version is not final since variables of the flask app need to follow the 'name' input of HTML file. I will modify them after HTML pages complete. 
    Since the flask needs to import ssh.py, ssh.py needs to be moved in the same directory.

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from . import ssh
 
 app = Flask(__name__)
-
 @app.route("/")
 def home():
     return render_template("home.html")
@@ -10,9 +10,17 @@ def home():
 def contact():
     return render_template("contact.html")
 
-@app.route("/settings")
+@app.route("/settings", methods=['POST', 'GET'])
 def settings():
-    return render_template("settings.html")
+    if request.method == "POST":
+        UV_mode_value = request.form['UV_mode']     # 'UV_mode' need to be same as the value of 'name' input in HTML file
+        Auto_mode_value = request.form['Auto_mode'] #  Same as above
+        Hour_value = int(request.form['Hours'])     #  Same as above 
+        Minute_value = int(request.form['Minutes']) #  Same as above
+        Sec_value = int(request.form['Seconds'])    #  Same as above
+        ssh.uv_on_off(UV_mode_value)                #  Turn on or off UV
+        ssh.run_auto_mode((Hour_value * 3600 + Minute_value * 60 + Sec_value), Auto_mode_value) # Make sure runtime is in seconds
+    return render_template("settings.html")         
 
 @app.route("/login")
 def login():
